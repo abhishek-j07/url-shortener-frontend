996aa8161c233937f67e9a6407e878420e8a3a06
abhishekjohri <abhishekjohri098@gmail.com>
2025-10-18 11:01:14 +0530

    Added Register user functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import AboutPage from './components/AboutPage'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import RegisterPage from './components/RegisterPage'
+import { Toaster } from 'react-hot-toast'
+import Login from './components/Login'
 
 function App() {
   
@@ -13,10 +15,12 @@ function App() {
   <>
     <Router>
     <NavBar/>
+    <Toaster position='bottom-center'/>
     <Routes>
       <Route path='/' element={<LandingPage/>}/>
       <Route path='/about' element={<AboutPage/>}/>
       <Route path='/register' element={<RegisterPage/>}/>
+      <Route path='/login' element={<Login/>}/>
     </Routes>
     <Footer/>
     </Router>
--- src/components/RegisterPage.jsx
+++ src/components/RegisterPage.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { data } from 'react-router-dom';
+import { data, Link, useNavigate } from 'react-router-dom';
+import TextField from './TextField';
+import api from '../api/api';
+import toast from 'react-hot-toast';
 
 const RegisterPage = () => {
+
+    const navigate = useNavigate();
+
+    const [loader, setLoader] = useState(false)
 
     const {
         register,
@@ -20,7 +27,24 @@ const RegisterPage = () => {
 
     const registerHandler = async (data) => {
 
-    }
+        setLoader(true);
+        try {
+            const {data: response } = await api.post(
+                "/api/auth/public/register",
+                data
+            );
+
+            reset();
+            navigate("/login");
+            toast.success("Registration successful!")
+
+        } catch (error) {
+            console.log(error);
+            toast.error("Registration failed. Please try again.")
+        } finally{
+            setLoader(false);
+        }
+    };
 
   return (
     <div
@@ -33,8 +57,55 @@ const RegisterPage = () => {
             <hr className='mt-2 mb-5 text-black'></hr>
 
             <div className='flex flex-col gap-3'>
-                
+                <TextField
+                label="Username"
+                required
+                id="username"
+                type="text"
+                message="Username is required"
+                placeholder="Type your username"
+                register={register}
+                errors={errors}/>
+
+                <TextField
+                label="Email"
+                required
+                id="email"
+                type="email"
+                message="Email is required"
+                placeholder="Type your email"
+                register={register}
+                errors={errors}/>
+
+                <TextField
+                label="Password"
+                required
+                id="password"
+                type="password"
+                message="Password is required"
+                placeholder="Type your password"
+                register={register}
+                min={6}
+                errors={errors}/>
             </div>
+
+            <button
+            disabled={loader}
+            type='submit'
+            className='bg-customRed font-semibold text-white  bg-custom-gradient w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3'
+            >
+                {loader ? "Loading..." : "Register"}
+            </button>
+
+            <p className='text-center text-sm text-slate-700 mt-2'>
+                Already have an account? 
+                <Link
+                className='font-semibold underline hover:text-black'
+                to="/login">
+                <span className='text-btnColor'> Login</span>
+                </Link>
+            </p>
+
         </form>
         </div>
   )
