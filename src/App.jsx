5d0c4232731fe93ba7d419fe591cb1219a698370
abhishekjohri <abhishekjohri098@gmail.com>
2025-10-23 20:37:20 +0530

    Added redirect to orignal Url functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
-import { useState } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import LandingPage from './components/LandingPage'
-import AboutPage from './components/AboutPage'
-import NavBar from './components/NavBar'
-import Footer from './components/Footer'
-import RegisterPage from './components/RegisterPage'
-import { Toaster } from 'react-hot-toast'
-import Login from './components/LoginPage'
-import DashboardLayout from './components/Dashboard/DashboardLayout'
-import LoginPage from './components/LoginPage'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { getApps } from './utils/helper';
 
 function App() {
-  
+
+  const CurrentApp = getApps();
+
   return (
-  <>
+    <>
     <Router>
-    <NavBar/>
-    <Toaster position='bottom-center'/>
-    <Routes>
-      <Route path='/' element={<LandingPage/>}/>
-      <Route path='/about' element={<AboutPage/>}/>
-      <Route path='/register' element={<RegisterPage/>}/>
-      <Route path='/login' element={<LoginPage/>}/>
-      <Route path='/dashboard' element={<DashboardLayout/>}/>
-    </Routes>
-    <Footer/>
+      <CurrentApp />
     </Router>
-  </>
+    </>
   )
 }
 
